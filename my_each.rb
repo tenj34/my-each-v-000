98ccf505a3ae5a276744e6b80d78aae0ab832155
Juan Tenezaca <tenj34@gmail.com>
2018-07-14 17:55:11 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(party)# put argument(s) here
+
+  i = 0
+  while i < party.length
+    yield array[i]
+    i =+ 1
+  end
+  array
+end
+
+collection = [1,2,3,4]
+my_each(collection) do |i|
+  puts i
 end
