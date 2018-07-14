c56f1b025e905f8c19169089aadaf1199b426f80
Juan Tenezaca <tenj34@gmail.com>
2018-07-14 17:57:26 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -2,13 +2,8 @@ def my_each(party)# put argument(s) here
 
   i = 0
   while i < party.length
-    yield my_party[i]
+    yield (my_party[i])
     i =+ 1
   end
   array
-end
-
-collection = [1,2,3,4]
-  my_each(collection) do |i|
-  puts i
 end
